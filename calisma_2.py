3352f939e743c4ad1e3c0a88249dc31b11d362ce
mehmettatar5821 <mehmettatar5821@gmail.com>
2026-02-24 23:37:00 +0300

    Create calisma_2.py

---
+++ calisma_2.py
@@ -0,0 +1,11 @@
+sayi=int(input("bir sayi giriniz: "))
+
+
+if sayi%2==0:
+    print(f"{sayi} sayisi cifttir. ")
+elif sayi < 0:
+    print("lütfen pozitif bir sayi giriniz. ")
+else:
+    print(f"{sayi} sayisi tektir. ")
+
+ 
