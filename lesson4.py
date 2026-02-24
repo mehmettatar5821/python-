9715c5888865876defbb70b9e9295a1c4603a73a
mehmettatar5821 <mehmettatar5821@gmail.com>
2026-02-24 17:59:55 +0300

    yaşı girilen kişinin ehliyet alıp alamayacağını karşıya çıktı veren program

---
+++ lesson4.py
@@ -0,0 +1,12 @@
+print("Program başladı")
+
+age = int(input("Yaşı giriniz: "))
+
+print("Girilen yaş:", age)
+
+if age >= 18:
+    print("Ehliyet alabilir")
+else:
+    print("Ehliyet alamaz")
+
+print("Program bitti")
