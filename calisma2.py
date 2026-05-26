1358639b899e7431c181cc29f22a14c49836e95f
MEHMET TATAR <mehmettatar5821@gmail.com>
2026-05-26 11:17:29 +0300

    Create calisma2.py

---
+++ calisma2.py
@@ -0,0 +1,8 @@
+# Ekrana yazı yazdırma
+print("Merhaba! Python dünyasına hoş geldin.")
+
+# Kullanıcıdan veri alma ve değişkene atama
+isim = input("Adın nedir? ")
+
+# Alınan veriyle kişiselleştirilmiş mesaj gösterme (f-string yöntemi)
+print(f"Tanıştığımıza memnun oldum, {isim}!")
